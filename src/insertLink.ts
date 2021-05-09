fbb60dade197a44cc96892882f896f97c6c62e2d
rctgardner <59621072+rctgardner@users.noreply.github.com>
2021-05-09 10:43:59 -0600

    allowing user to insert links without highlighting

--- src/insertLink.ts
+++ src/insertLink.ts
@@ -25,61 +25,72 @@ export function insertLink(editor: vscode.TextEditor|undefined, groups: Array<Gr
     }
     else {
         if (debug) { log(`insertLink: Getting currently highlighted text`); }
-        const currentSelection: vscode.Selection|undefined = editor.selection;
-        if (currentSelection === undefined || currentSelection.isEmpty) {
-            // we don't actually have text to replace
-            // ... this might happen if this command was called without highlighting text first
-            if (debug) { log(`insertLink: No text has been selected. Cannot insert link`); }
+        let currentSelection: vscode.Selection = editor.selection;
+        // user isn't highlighting anything - just has a cursor pointed at something
+        if (currentSelection.isEmpty) {
+            // Need to match all ATT&CK IDs generically, including sub-techniques, hence the period
+            // ... only matches 1 word - if the user needs to insert a link for multiple words (e.g. 'OS Credential Dumping')
+            // ... then they will need to highlight the entire term
+            const wordMatcher: RegExp = /[a-zA-Z0-9.]+/;
+            const cursorRange: vscode.Range|undefined = editor.document.getWordRangeAtPosition(currentSelection.active, wordMatcher);
+            // if cursorRange is undefined, most likely the selected text is not a word
+            // ... in this case, just carry through and let the highlightedText remain undefined
+            if (cursorRange !== undefined) {
+                currentSelection = new vscode.Selection(cursorRange.start, cursorRange.end);
+            }
+        }
+        const highlightedText: string|undefined = editor.document.getText(currentSelection);
+        if (highlightedText === undefined) {
+            log(`insertLink: No text has been selected. Cannot insert link`);
             vscode.window.showWarningMessage('ATT&CK: Could not insert a link, because no text was selected.');
+            return;
+        }
+        // text was found, now to figure out which ATT&CK object it identifies
+        const trimmedText = highlightedText.trimRight();
+        if (debug) { log(`insertLink: Text to insert a link for: '${trimmedText}'`); }
+        const configuration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(configSection);
+        let matchingObject: Group|Mitigation|Software|Tactic|Technique|undefined = undefined;
+        // search all ATT&CK types for the first matching ID
+        if (matchingObject === undefined && configuration.get('groups')) {
+            matchingObject = groups.find((g: Group) => { return g.id === trimmedText; });
+        }
+        if (matchingObject === undefined && configuration.get('mitigations')) {
+            matchingObject = mitigations.find((m: Mitigation) => { return m.id === trimmedText; });
+        }
+        if (matchingObject === undefined && configuration.get('software')) {
+            matchingObject = software.find((s: Software) => { return s.id === trimmedText; });
+        }
+        if (matchingObject === undefined && configuration.get('tactics')) {
+            matchingObject = tactics.find((t: Tactic) => { return t.id === trimmedText; });
+        }
+        if (matchingObject === undefined && configuration.get('techniques')) {
+            matchingObject = techniques.find((t: Technique) => { return t.id === trimmedText; });
+        }
+        if (matchingObject === undefined) {
+            // doesn't look like the highlighted text resembles any ATT&CK object we're aware of
+            if (debug) { log(`insertLink: Did not find a matching object for '${trimmedText}'`); }
+            vscode.window.showWarningMessage(`ATT&CK: Could not insert a link, because '${trimmedText.substr(0, 20)}' does not match any available ATT&CK objects.`);
+            return;
+        }
+        const link: string|undefined = generateLink(trimmedText, matchingObject.url);
+        if (link === undefined) {
+            // we should never get here since we should've detected malformed ATT&CK objects on startup
+            // ... but still, let's try to inform the user if something weird happens
+            log(`insertLink: Could not extract URL for ${trimmedText}!`);
+            vscode.window.showErrorMessage(`ATT&CK: An error occurred while attempting to insert a link - no associated URL found for ${trimmedText}!`);
         }
         else {
-            const highlightedText = editor.document.getText(currentSelection);
-            const trimmedText = highlightedText.trimRight();
-            if (debug) { log(`insertLink: Text to insert a link for: '${trimmedText}'`); }
-            const configuration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(configSection);
-            let matchingObject: Group|Mitigation|Software|Tactic|Technique|undefined = undefined;
-            // search all ATT&CK types for the first matching ID
-            if (matchingObject === undefined && configuration.get('groups')) {
-                matchingObject = groups.find((g: Group) => { return g.id === trimmedText; });
-            }
-            if (matchingObject === undefined && configuration.get('mitigations')) {
-                matchingObject = mitigations.find((m: Mitigation) => { return m.id === trimmedText; });
-            }
-            if (matchingObject === undefined && configuration.get('software')) {
-                matchingObject = software.find((s: Software) => { return s.id === trimmedText; });
-            }
-            if (matchingObject === undefined && configuration.get('tactics')) {
-                matchingObject = tactics.find((t: Tactic) => { return t.id === trimmedText; });
-            }
-            if (matchingObject === undefined && configuration.get('techniques')) {
-                matchingObject = techniques.find((t: Technique) => { return t.id === trimmedText; });
-            }
-            if (matchingObject === undefined) {
-                // doesn't look like the highlighted text resembles any ATT&CK object we're aware of
-                if (debug) { log(`insertLink: Did not find a matching object for '${trimmedText}'`); }
-                vscode.window.showWarningMessage(`ATT&CK: Could not insert a link, because '${trimmedText.substr(0, 20)}' does not match any available ATT&CK objects.`);
-                return;
-            }
-            const link: string|undefined = generateLink(trimmedText, matchingObject.url);
-            if (link === undefined) {
-                // we should never get here since we should've detected malformed ATT&CK objects on startup
-                // ... but still, let's try to inform the user if something weird happens
-                log(`insertLink: Could not extract URL for ${matchingObject.id}!`);
-                vscode.window.showErrorMessage(`ATT&CK: An error occurred while attempting to insert a link - no associated URL found for ${matchingObject.id}!`);
-            }
-            else {
-                // and we're finally at the point where a link can be inserted
-                editor.edit((editBuilder: vscode.TextEditorEdit) => {
-                    if (debug) { log(`insertLink: Found a matching object. Inserting the following text: '${link}'`); }
-                    if (trimmedText !== highlightedText) {
-                        // append extra whitespace if there is any
-                        editBuilder.replace(currentSelection, link + highlightedText.replace(trimmedText, ''));
-                    }
-                    else {
-                        editBuilder.replace(currentSelection, link);
-                    }
-                });
-            }
+            // and we're finally at the point where a link can be inserted
+            editor.edit((editBuilder: vscode.TextEditorEdit) => {
+                if (debug) { log(`insertLink: Found a matching object. Inserting the following text: '${link}'`); }
+                if (highlightedText !== trimmedText) {
+                    // append extra whitespace if there is any
+                    editBuilder.replace(currentSelection, link + highlightedText.replace(trimmedText, ''));
+                }
+                else {
+                    editBuilder.replace(currentSelection, link);
+                }
+            });
         }
     }
 }
