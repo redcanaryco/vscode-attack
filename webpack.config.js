bca2059e9492744834d64d9d15d16f6355ceb6e8
rctgardner <59621072+rctgardner@users.noreply.github.com>
2022-02-11 13:47:54 -0700

    add simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+/** @type WebpackConfig */
+const webExtensionConfig = {
+  mode: 'none',
+  target: 'webworker',
+  entry: {
+    extension: './src/extension.ts',
+    'test/suite/index': './src/test/suite/index.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './dist/web'),
+    libraryTarget: 'commonjs',
+    devtoolModuleFilenameTemplate: '../../[resource-path]'
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.ts', '.js'],
+    alias: {},
+    fallback: {
+        assert: require.resolve('assert')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ],
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'nosources-source-map'
+};
+const nodeExtensionConfig = {
+  mode: 'none',
+  target: 'webworker',
+  entry: {
+    extension: './src/extension.ts',
+    'test/suite/index': './src/test/suite/index.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './dist/host'),
+    libraryTarget: 'commonjs',
+    devtoolModuleFilenameTemplate: '../../[resource-path]'
+  },
+  resolve: {
+    mainFields: ['module', 'main'],
+    extensions: ['.ts', '.js'],
+    alias: {},
+    fallback: {
+        assert: require.resolve('assert')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ],
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'nosources-source-map'
+};
+
+module.exports = [webExtensionConfig, nodeExtensionConfig];
